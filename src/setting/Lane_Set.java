3d637576a3ce34b368428c0b200bc5c791d70107
Gary Chen <hchen20@taboracademy.org>
2020-05-01 01:18:51 -0400

    I changed it back. I need time to think about intersection

--- src/setting/Lane_Set.java
+++ src/setting/Lane_Set.java
@@ -14,23 +14,23 @@ import traffic_congestion_simulator.TCSConstant;
  */
 public class Lane_Set implements TCSConstant{
     
-    Lane[] lane_set;
+    Lane2[] lane_set;
     
     public Lane_Set(boolean automated, int[] x_value, int[] y_value, int[]length, int[] width, int[] direction, int numOfLane, Light_Set light_set) {
-        lane_set = new Lane[numOfLane];
+        lane_set = new Lane2[numOfLane];
         if (light_set != null) {
             for (int i = 0; i < lane_set.length; i++) {
-                lane_set[i] = new Lane(automated, x_value[i], y_value[i], length[i], width[i], direction[i], light_set.getLight_Set()[i]);
+                lane_set[i] = new Lane2(automated, x_value[i], y_value[i], length[i], width[i], direction[i], light_set.getLight_Set()[i]);
             }
         } else {
             for (int i = 0; i < lane_set.length; i++) {
-                lane_set[i] = new Lane(automated, x_value[i], y_value[i], length[i], width[i], direction[i], null);
+                lane_set[i] = new Lane2(automated, x_value[i], y_value[i], length[i], width[i], direction[i], null);
             }
         }
     }
     
     public void runUnit() {
-        for (Lane lane_set1 : lane_set) {
+        for (Lane2 lane_set1 : lane_set) {
             lane_set1.runUnit();
         }
     }
