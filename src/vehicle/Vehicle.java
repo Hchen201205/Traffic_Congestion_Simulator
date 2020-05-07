44acea6b3fec993d87c2d628aaaf601c604c75c4
Gary Chen <hchen20@taboracademy.org>
2020-05-07 01:22:40 -0400

    Finally, estimateDistance for the automated Vehicle is here
    Check it out. I derived the equation and this comes up. Mathematically, it's correct. Feel free to check it for me. I think the timing won't be an issue because it is essentially Math. It has really little of using other classes or accessing method

--- src/vehicle/Vehicle.java
+++ src/vehicle/Vehicle.java
@@ -14,7 +14,8 @@ import traffic_congestion_simulator.TCSConstant;
  *
  * @author Christine
  */
-public abstract class Vehicle implements  TCSConstant {
+public abstract class Vehicle implements TCSConstant {
+
     protected double[] speed = new double[2];//{speed left/right, speed up/down} in m/s (always positive)               
     protected double[] position;             //Position as the middle vehicle, in m
     protected double[] size;                 //{length, width} in m
@@ -25,46 +26,43 @@ public abstract class Vehicle implements  TCSConstant {
     protected double safety_distance;        //distance needed for car to safely decelerate to 0
     protected double time_moving;            //total time vehicle has been moving, helpful for testing/checking
     protected double reaction_time;
-    
+
     protected boolean is_accelerating;       //true if accelerate method is running
     protected boolean is_turning;            //true if car is turning in intersection
-    
+
     //temporary turning constants (only called before a car starts a turn)
     protected double turning_acceleration;   //acceleration value used only for turning
     protected double turn_radius;            //radius of quarter circle modeling turn
     protected double turning_velocity;       //calculated from turning_acceleration value
     protected double[] turn_initial_position;//position before turn (with center at limit line)
     protected double turn_initial_direction; //direction car was facing before turn
-    
+
     protected Random rand = new Random(100); // Instead of initialize random in each car class, it can be created here.
-    
+
     protected final double buffer = BUFFER;  //gap between cars when stopped, in m
     protected final int rounded_dec_pos = ROUNDEDDECPOS;     //the decimal position accuracy of functions
     protected final double time_increments = TIMEINCREMENTS; //milliseconds 
-    
-    
-    
+
     //Abstract functions:
-    
     //simple acceleration function, updates position, speed, saftey_distance, time_moving
     //accelerate = true to accelerate & accelerate = false to decelerate
     public abstract void accelerate(double time, boolean accelerate);
-    
+
     //calculates and assigns new saftey distance based on current speed
     public abstract void updateSafetyDistance();
-    
+
     //assigns each car a random acceleration rate
     public abstract void genRandAcceleration();
-    
+
     //assigns each car a random deceleration rate
     public abstract void genRandDeceleration();
-    
+
     public abstract void genRandReactionTime();
-    
+
     //used when turn() is first called, sets starting constants so the function 
     //can be called on many times and edit Vehicle vairables
     public abstract void setTurningConstants(double[] destination, boolean accelerate);
-    
+
     //      - Direction is 90 for left turn and -90 for right turn
     //      - Destination is the middle of the limit line of the lane the car should turn to 
     //      - Accounts for the back half of the car still being in the intersection 
@@ -75,10 +73,12 @@ public abstract class Vehicle implements  TCSConstant {
     // has finished the turn, is_turning will be set to false
     //accelerate = true to accelerate & accelerate = false to decelerate
     public abstract void turn(int direction, double[] destination, boolean accelerate);
-    
+
     //distance between front bumper of vehicle to back bumper of front car plus the buffer
     public abstract double getDistanceFromFrontVehicle(Vehicle front_car);
-    
+
+    public abstract boolean getAutomated();
+
     // this will get a point that is the center of width but the front of one side of the vehicle
     public double[] getCarFrontPos() {
         double[] frontPos = new double[2];
@@ -86,64 +86,58 @@ public abstract class Vehicle implements  TCSConstant {
         frontPos[1] = this.rounder(position[1] + 1 / 2 * size[0] * Math.sin(Math.toRadians(direction)));
         return frontPos;
     }
-    
+
     //no implementation yet
     //for use when car is turning
     public abstract double getDistanceFromTurningVehicle(Vehicle front_car);
-    
+
     //no implementation yet
     //returns distance needed to reach the limit line of the lane (begining of intersection)
-    public abstract double getDistanceFromLimitLine (Lane lane);
-    
-    
-    
+    public abstract double getDistanceFromLimitLine(Lane lane);
+
     //Non-abstract functions:
-    
     //returns true vehicle is facing east or west
-    public boolean isTravelingHorizontal(){
-        if (direction == 0 || direction == 180){
+    public boolean isTravelingHorizontal() {
+        if (direction == 0 || direction == 180) {
             return true;
-        } 
+        }
         return false;
     }
-    
+
     /* Once we have turning. I will implement this method.
     public void genRandDestination() {
         
     }
-    */
-    
-    public boolean isTravelingVertical(){
-        if (direction == 90 || direction == 270){
+     */
+    public boolean isTravelingVertical() {
+        if (direction == 90 || direction == 270) {
             return true;
-        } 
+        }
         return false;
     }
-    
-    public double rounder (double num){
+
+    public double rounder(double num) {
         num = num * Math.pow(10, rounded_dec_pos);
         num = Math.round(num);
         return num / Math.pow(10, rounded_dec_pos);
     }
 
-    
-    
     //Basic getters
-    
     public boolean isAccelerating() {
         return is_accelerating;
     }
-    
-    public boolean isTurning(){
+
+    public boolean isTurning() {
         return is_turning;
     }
-    
-    public boolean isStopped(){
-        if (this.getDirectionalSpeed() == 0){
+
+    public boolean isStopped() {
+        if (this.getDirectionalSpeed() == 0) {
             return true;
         }
         return false;
     }
+
     /*
     public boolean isAtLimitLine(){
         //no implementation yet
@@ -153,36 +147,37 @@ public abstract class Vehicle implements  TCSConstant {
         }
             return false;    
     }
-    */
-    public double getDirection(){
+     */
+    public double getDirection() {
         return direction;
     }
+
     //returns char value for direction
     //'!' means car is not travelling in one of the cardinal directions
-    public char getCharDirection(){
-        switch((int) direction) {
+    public char getCharDirection() {
+        switch ((int) direction) {
             case 0:
                 return 'r';
             case 90:
                 return 'u';
             case 180:
                 return 'l';
-            case 270: 
+            case 270:
                 return 'd';
             default:
                 return '!';
         }
     }
-    
+
     public double[] getPosition() {
         return position;
     }
-    
+
     //the position component that is changing
-    public double getDirectionalPos(){
-        if (direction ==  0|| direction == 180){
+    public double getDirectionalPos() {
+        if (direction == 0 || direction == 180) {
             return this.position[0];
-        } else if (direction == 90 || direction == 270){
+        } else if (direction == 90 || direction == 270) {
             return this.position[1];
         }
         //leaving this in as an error message in case
@@ -190,12 +185,12 @@ public abstract class Vehicle implements  TCSConstant {
         System.out.println("This vehicle is not facing a cardinal direction");
         return 0;
     }
-    
+
     //the speed component that is changing
-    public double getDirectionalSpeed(){
-        if (direction ==  0|| direction == 180){
+    public double getDirectionalSpeed() {
+        if (direction == 0 || direction == 180) {
             return this.speed[0];
-        } else if (direction == 90 || direction == 270){
+        } else if (direction == 90 || direction == 270) {
             return this.speed[1];
         }
         //leaving this in as an error message in case
@@ -203,7 +198,7 @@ public abstract class Vehicle implements  TCSConstant {
         System.out.println("This vehicle is not traveling in a cardinal direction");
         return 0;
     }
-    
+
     public double getSafetyDistance() {
         return safety_distance;
     }
@@ -211,34 +206,30 @@ public abstract class Vehicle implements  TCSConstant {
     public double[] getSize() {
         return size;
     }
-    
-    public double getTimeMoving(){
+
+    public double getTimeMoving() {
         return time_moving;
     }
-    
+
     public double getBuffer() {
         return buffer;
     }
-    
-    public int getRoundedDecPos(){
+
+    public int getRoundedDecPos() {
         return rounded_dec_pos;
     }
-    
+
     public double getReactionTime() {
         return reaction_time;
     }
-    
+
     public void reduceReactionTimeUnit() {
         reaction_time -= TIMEINCREMENTS;
     }
-    
 
-    
-    
     //The following functions are now obsolete with the way Vehicle classes function
     //But the logic is still helpful and might be able to be reused in other classes
     //See AutomatedCar for fleshed out functions and code
-    
     /*
     
     public abstract void accelerateToSpeed(double speed) throws InterruptedException;
@@ -252,6 +243,5 @@ public abstract class Vehicle implements  TCSConstant {
     //vehicle will travel a certain distance to a stop, accelerating to speed limit 
     //until it needs to begin decelerating based on saftey distance 
     public abstract void travelDistanceToStop(double distance) throws InterruptedException;
-    */
-
+     */
 }
