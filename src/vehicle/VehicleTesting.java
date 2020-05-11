c24aedf8ca24290d5c90c650534db7a3d0afd35b
Steegesaurus45 <63888457+Steegesaurus45@users.noreply.github.com>
2020-05-11 13:21:15 -0700

    Getting data for practice graphing
    just grabbing some car data for practice graphing in matlab

--- src/vehicle/VehicleTesting.java
+++ src/vehicle/VehicleTesting.java
@@ -7,12 +7,13 @@ package vehicle;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import traffic_congestion_simulator.TCSConstant;
 
 /**
  *
  * @author Christine
  */
-public class VehicleTesting {
+public class VehicleTesting implements TCSConstant{
 
     public static void accelerateTesting() {
 
@@ -21,16 +22,16 @@ public class VehicleTesting {
     public static void turnTesting() {
         double[] size = {2, 1};
         double[][] pos = {{2, 5}, {2, 0.5}, {5.5, 0}, {7.5, 0}, {8, 3.5}, {8, 5.5}, {4.5, 6}, {2.5, 6}};
-        double[][] des = {{9, 12}, {2.5, 0}, {2,  3.5}, {8,  0.5}, {4.5, 0}, {7.5, 6}, {8, 2.5}, {2, 5.5}};
+        double[][] des = {{9, 12}, {2.5, 0}, {2, 3.5}, {8, 0.5}, {4.5, 0}, {7.5, 6}, {8, 2.5}, {2, 5.5}};
 
-        HashMap<Integer, String> facing = new HashMap<Integer, String>();
-        facing.put(0, "right");
-        facing.put(90, "up");
-        facing.put(180, "left");
-        facing.put(270, "down");
+        HashMap<Double, String> facing = new HashMap<Double, String>();
+        facing.put(0.0, "right");
+        facing.put(90.0, "up");
+        facing.put(180.0, "left");
+        facing.put(270.0, "down");
 
         for (int i = 0; i < 8; i++) {
-            int direction = 0;
+            double direction = 0;
             direction += ((int) ((i) / 2.0)) * 90;
             AutomatedCar car = new AutomatedCar(pos[i], size, direction);
 
@@ -53,7 +54,7 @@ public class VehicleTesting {
             while (car.is_turning) {
                 count++;
                 car.turn(turn, des[i], true);
-                if (count % 20 == 0) {
+                if (count % 1 == 0) {
                     System.out.println("Position: [" + car.rounder(car.position[0])
                            + ", " + car.rounder(car.position[1]) + "]");
                     System.out.println("Direction: " + car.rounder(car.direction));
@@ -63,7 +64,45 @@ public class VehicleTesting {
             System.out.println("Final Direction: " + car.direction);
             System.out.println("Final Position: " + Arrays.toString(car.position));
             System.out.println("\n\n\n\n\n\n");
-        
-       }
+
+        }
     }
+
+    public void graphDataTesting() {
+        double[] size = {2, 1};
+        double[] pos = {2, 5};
+        double[] des = {9, 12};
+        double direction = 0;
+        int turn = 90;
+        
+        AutomatedCar car = new AutomatedCar(pos, size, direction);
+        
+        double[] pos2 = {0, 3};
+        AutomatedCar car2 = new AutomatedCar(pos2, size, direction);
+        
+
+        System.out.println("Left turn facing right:");
+        System.out.println("----------------------------------------");
+
+        System.out.println("Initial Position: " + Arrays.toString(car.position));
+        car.turn(turn, des, true);
+        System.out.println("Turn radius: " + car.turn_radius);
+
+        System.out.println("\nTurning: ");
+        double count = 0;
+        while (car.is_turning) {
+            count++;
+            car.turn(turn, des, true);
+            car2.accelerate(TIMEINCREMENTS, true);
+            System.out.println(car.rounder(car.position[0])
+                    + ", " + car.rounder(car.position[1]) + "\t" + car.rounder(car2.position[0])
+                    + ", " + car.rounder(car2.position[1]));
+
+        }
+        
+        System.out.println("\nTime increments: " + count);
+        System.out.println("Final Direction: " + car.direction);
+        System.out.println("Final Position: " + Arrays.toString(car.position));
+    }
+
 }
