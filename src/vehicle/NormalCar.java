64e0d094037bea6d6d62bd59ce1cc7a11259d527
Steegesaurus45 <63888457+Steegesaurus45@users.noreply.github.com>
2020-05-17 06:45:07 -0700

    fixing accelerate
    The math for the normal accelerate function was different for some reason. I changed it so it is the same as Automated.

--- src/vehicle/NormalCar.java
+++ src/vehicle/NormalCar.java
@@ -135,13 +135,13 @@ public class NormalCar extends Vehicle implements TCSConstant {
         is_accelerating = true;
 
         position[0] += (speed[0] * TCSConstant.TIMEINCREMENTS + 1.0 / 2 * acceleration 
-        * TCSConstant.TIMEINCREMENTS * TCSConstant.TIMEINCREMENTS) * Math.cos(Math.toRadians(direction));
+                * TCSConstant.TIMEINCREMENTS * TCSConstant.TIMEINCREMENTS) * Math.cos(Math.toRadians(direction));
         position[0] = this.rounder(position[0]);
         speed[0] += acceleration * TCSConstant.TIMEINCREMENTS * Math.abs(Math.cos(Math.toRadians(direction)));
         speed[0] = this.rounder(speed[0]);
 
         position[1] += (speed[1] * TCSConstant.TIMEINCREMENTS + 1.0 / 2 * acceleration 
-        * TCSConstant.TIMEINCREMENTS * TCSConstant.TIMEINCREMENTS) * Math.sin(Math.toRadians(direction));;
+                * TCSConstant.TIMEINCREMENTS * TCSConstant.TIMEINCREMENTS) * Math.sin(Math.toRadians(direction));;
         position[1] = this.rounder(position[1]);
         speed[1] += acceleration * TCSConstant.TIMEINCREMENTS * Math.abs(Math.sin(Math.toRadians(direction)));
         speed[1] = this.rounder(speed[1]);
